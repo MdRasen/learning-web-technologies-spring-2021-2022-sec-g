469b6322af09d68a4d7302745b5ef8c8122abe02
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2022-02-13 11:55:47 +0600

    Files updated

---
+++ MID_LAB_TASK_05_PHP_FORM/Task01/A/Task01.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Task 01</title>
+</head>
+
+<body>
+
+    <form method="post" action="Check.php">
+		<fieldset>
+			<legend>NAME</legend>
+            <input type="text" name="name" value=""><br/><hr>
+            <input type="submit" name="submit" value="Submit">
+         
+		</fieldset>
+	</form>
+
+</body>
+
+</html>
---
+++ MID_LAB_TASK_05_PHP_FORM/Task01/B/Task01.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+    <title>Task 01</title>
+</head>
+
+<body>
+
+    <form method="post" action="check.php">
+        <table>
+            <tr>
+                <td>Name: </td>
+                <td>
+                    <input type="text" name="name" value="">
+                </td>
+            </tr>
+
+            <tr>
+				<td></td>
+				<td>
+					<input type="submit" name="submit" value="Submit">
+				</td>
+			</tr>
+        </table>
+    </form>
+
+</body>
+
+</html>
