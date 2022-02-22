f4bd3ce3eb6caf4dd63908c54a0ecf6d81b8e13b
Md Al Amin Hossen <aamin.hossen99@gmail.com>
2022-02-22 10:29:06 +0600

    Task 06 added

---
+++ MID_LAB_TASK_PHP_CURD_06/views/home.php
@@ -0,0 +1,24 @@
+<?php 
+	//include("header.php");
+	//include_once("header.php");
+
+	require('header.php');
+	//require_once('header.php');
+?>
+
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+	<h1>Welcome home!</h1> 
+
+	<a href="create.php"> Create User</a> |
+	<a href="create.php"> Read User</a> |
+	<a href="create.php"> Update User</a> |
+	<a href="create.php"> Delete User</a> |
+	<a href="userlist.php"> User List</a> |
+	<a href="../controllers/logout.php"> logout</a>
+
+</body>
+</html>
